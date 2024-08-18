c65b0a36a431f46734cfdc2d99eafd4e5d093bb9
Bohdan <protzzz.it@gmail.com>
2024-08-18 21:25:19 +0200

    +styles - styles file

--- lib/core/res/styles/app_styles.dart
+++ lib/core/res/styles/app_styles.dart
@@ -5,11 +5,17 @@ Color primary = const Color(0xFF687daf);
 class AppStyles {
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+
+  static TextStyle textStyle =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: textColor);
 
   static TextStyle headLineStyle1 =
-      TextStyle(
-        fontSize: 26, 
-        fontWeight: FontWeight.bold,
-        color: textColor
-      );
+      TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: textColor);
+
+  static TextStyle headLineStyle2 =
+      TextStyle(fontSize: 17, fontWeight: FontWeight.bold, color: textColor);
+
+  static TextStyle headLineStyle3 =
+      TextStyle(fontSize: 21, fontWeight: FontWeight.bold, color: textColor);
 }
