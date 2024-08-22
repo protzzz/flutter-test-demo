51f58e20da5f5bc5860c69a612ca87debd5d4e5a
Bohdan <protzzz.it@gmail.com>
2024-08-22 02:19:44 +0200

    Added: all_json

---
+++ lib/screens/all_json.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "NYC", 'name': "New-York"},
+    'to': {'code': "LDN", 'name': "London"},
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    'number': 23
+  },
+  {
+    'from': {'code': "LAX", 'name': "Los Angeles"},
+    'to': {'code': "SYD", 'name': "Sydney"},
+    'flying_time': '15H 20M',
+    'date': "15 JUNE",
+    'departure_time': "10:45 PM",
+    'number': 78
+  },
+  {
+    'from': {'code': "CDG", 'name': "Paris"},
+    'to': {'code': "DXB", 'name': "Dubai"},
+    'flying_time': '6H 50M',
+    'date': "12 SEPTEMBER",
+    'departure_time': "04:30 PM",
+    'number': 45
+  },
+  {
+    'from': {'code': "HND", 'name': "Tokyo"},
+    'to': {'code': "JFK", 'name': "New York"},
+    'flying_time': '14H 00M',
+    'date': "22 NOVEMBER",
+    'departure_time': "01:15 PM",
+    'number': 110
+  },
+  {
+    'from': {'code': "SIN", 'name': "Singapore"},
+    'to': {'code': "AMS", 'name': "Amsterdam"},
+    'flying_time': '13H 10M',
+    'date': "3 DECEMBER",
+    'departure_time': "11:55 PM",
+    'number': 56
+  },
+  {
+    'from': {'code': "ORD", 'name': "Chicago"},
+    'to': {'code': "FRA", 'name': "Frankfurt"},
+    'flying_time': '8H 45M',
+    'date': "30 JULY",
+    'departure_time': "07:30 AM",
+    'number': 89
+  }
+];
