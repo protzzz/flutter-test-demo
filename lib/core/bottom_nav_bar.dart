cf6a4257adcacc5813a28006f0314cc90154af54
Bohdan <protzzz.it@gmail.com>
2024-08-23 23:12:03 +0200

    Updated: search screen

--- lib/core/bottom_nav_bar.dart
+++ lib/core/bottom_nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:test_app/screens/home_screen.dart';
+import 'package:test_app/screens/home/home_screen.dart';
+import 'package:test_app/screens/search/search_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -13,7 +14,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   final appScreens = [
     // const Center(child: Text("Home")),
     const HomeScreen(),
-    const Center(child: Text("Search")),
+    const SearchScreen(),
     const Center(child: Text("Tickets")),
     const Center(child: Text("Profile"))
   ];
