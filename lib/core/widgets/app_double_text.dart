f4227e7257e676bda82feb0d17a779f55ffa6e4d
Bohdan <protzzz.it@gmail.com>
2024-08-23 05:38:08 +0200

    Added: navigation to "View All" list

--- lib/core/widgets/app_double_text.dart
+++ lib/core/widgets/app_double_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/core/res/styles/app_styles.dart';
+import 'package:test_app/screens/all_tickets.dart';
 
 class AppDoubleText extends StatelessWidget {
   const AppDoubleText({super.key, required this.bigText, required this.smallText});
@@ -15,7 +16,10 @@ class AppDoubleText extends StatelessWidget {
         Text(bigText, style: AppStyles.headLineStyle3),
         InkWell(
           onTap: () => {
-
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: ((BuildContext context) => const AllTickets()))
+            )
           },
           child: Text(smallText, style: AppStyles.textStyle.copyWith(
             color: AppStyles.primaryColor
