29649f70e79e139f0a33db5053868dabf04c17ea
Bohdan <protzzz.it@gmail.com>
2024-08-23 20:46:17 +0200

    Created: Tickets list view

--- lib/screens/all_tickets.dart
+++ lib/screens/all_tickets.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/core/utils/all_json.dart';
+import 'package:test_app/core/widgets/ticket_view.dart';
 
 class AllTickets extends StatelessWidget {
   const AllTickets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All Tickets"),
+      ),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: ticketList
+                  .map((singltTicket) =>
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        child: TicketView(ticket: singltTicket, wholeScreen: true,)
+                      )
+                    )
+                  .toList(),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
