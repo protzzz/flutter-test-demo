756ecdae8fbabae7e57d2d08dcc27e913ab5d50c
Bohdan <protzzz.it@gmail.com>
2024-08-23 22:22:53 +0200

    Updated: added func variable

--- lib/core/widgets/app_double_text.dart
+++ lib/core/widgets/app_double_text.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/core/res/styles/app_styles.dart';
-import 'package:test_app/screens/all_tickets.dart';
 
 class AppDoubleText extends StatelessWidget {
-  const AppDoubleText({super.key, required this.bigText, required this.smallText});
+  const AppDoubleText(
+      {super.key,
+      required this.bigText,
+      required this.smallText,
+      required this.func});
 
   final String bigText;
   final String smallText;
+  final VoidCallback func;
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +19,11 @@ class AppDoubleText extends StatelessWidget {
       children: [
         Text(bigText, style: AppStyles.headLineStyle3),
         InkWell(
-          onTap:() => Navigator.pushNamed(context, "/all_tickets"),
-          child: Text(smallText, style: AppStyles.textStyle.copyWith(
-            color: AppStyles.primaryColor
-          ),),
+          onTap: func,
+          child: Text(
+            smallText,
+            style: AppStyles.textStyle.copyWith(color: AppStyles.primaryColor),
+          ),
         )
       ],
     );
