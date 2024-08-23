6a18afa93c4876e644ce67d4bb79fe5e1b780d96
Bohdan <protzzz.it@gmail.com>
2024-08-24 01:33:48 +0200

    Updated: search_screen

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/core/res/styles/app_styles.dart';
+import 'package:test_app/screens/search/widgets/app_ticket_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          const SizedBox(height: 40),
+          Text(
+            "What are\nyou looking for?",
+            style: AppStyles.headLineStyle1.copyWith(fontSize: 35),
+          ),
+          const SizedBox(height: 20),
+          const AppTicketTabs(),
+
+        ],
+      ),
+    );
   }
 }
