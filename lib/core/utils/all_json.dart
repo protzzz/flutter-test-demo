932e050fb8a0e914d2486e1e5df9efab2bf82755
Bohdan <protzzz.it@gmail.com>
2024-08-23 23:12:17 +0200

    Updated: new hotel list

--- lib/core/utils/all_json.dart
+++ lib/core/utils/all_json.dart
@@ -48,3 +48,24 @@ List<Map<String, dynamic>> ticketList = [
     'number': 89
   }
 ];
+
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotel_room.png',
+    'place': 'One Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'city_view.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'pool_view.png',
+    'place': 'Best city pool',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
