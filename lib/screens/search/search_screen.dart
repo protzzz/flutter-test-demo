1c06550ab770a8749c4bfcbcbf1b6e502f534231
Bohdan <protzzz.it@gmail.com>
2024-08-24 21:26:43 +0200

    Updated: 1 and 2 tabs are done

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/core/res/media.dart';
 import 'package:test_app/core/res/styles/app_styles.dart';
 import 'package:test_app/core/widgets/app_double_text.dart';
 import 'package:test_app/screens/search/widgets/app_text_icon.dart';
@@ -43,25 +44,84 @@ class SearchScreen extends StatelessWidget {
           ),
           const SizedBox(height: 15),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                 width: size.width * .42,
+                height: 405,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.white,
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.shade200,
-                        blurRadius: 3,
-                        spreadRadius: 4
-                      )
-                    ]
-                ),
+                          color: Colors.grey.shade200,
+                          blurRadius: 3,
+                          spreadRadius: 4)
+                    ]),
                 child: Column(
-                  children: [],
+                  children: [
+                    Container(
+                      height: 190,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(AppMedia.planeSit))),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      "20% discount on the early booking of this flight. Don't miss!",
+                      style: AppStyles.headLineStyle4,
+                    )
+                  ],
                 ),
-              )
+              ),
+              Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                        width: size.width * .44,
+                        height: 230,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                            color: Color(0xFF3AB8B8)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount\nfor survey",
+                              style: AppStyles.headLineStyle3.copyWith(
+                                  fontWeight: FontWeight.bold, color: Colors.white),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Take the survey about our services and get discount",
+                              style: AppStyles.headLineStyle3.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  fontSize: 18),
+                            )
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -40,
+                        child: Container(
+                          padding: const EdgeInsets.all(30),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 18,
+                              color: AppStyles.circleColor
+                            ),
+                          ), child: null,
+                        ),
+                      )
+                    ],
+                  ),
             ],
           ),
         ],
