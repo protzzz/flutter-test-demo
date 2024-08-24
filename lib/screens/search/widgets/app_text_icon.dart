05e5fa8328bd85afaf8aa171d5512cc552802690
Bohdan <protzzz.it@gmail.com>
2024-08-24 03:17:38 +0200

    Finished: app_text_icon

--- lib/screens/search/widgets/app_text_icon.dart
+++ lib/screens/search/widgets/app_text_icon.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/core/res/styles/app_styles.dart';
 
 class AppTextIcon extends StatelessWidget {
-  const AppTextIcon({super.key});
+  const AppTextIcon({super.key, required this.icon, required this.text});
+  final IconData icon;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Colors.white),
+      child: Row(
+        children: [
+          Icon(icon, color: AppStyles.planeColor),
+          const SizedBox(width: 10),
+          Text(
+            text,
+            style: AppStyles.textStyle,
+          )
+        ],
+      ),
+    );
   }
 }
