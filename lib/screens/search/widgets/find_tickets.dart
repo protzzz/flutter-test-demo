a5c1c9be4758edc0577cfda7e04a8823502d95c7
Bohdan <protzzz.it@gmail.com>
2024-08-24 03:16:48 +0200

    Added: find_tickets

---
+++ lib/screens/search/widgets/find_tickets.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/core/res/styles/app_styles.dart';
+
+class FindTickets extends StatelessWidget {
+  const FindTickets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 18),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppStyles.findTicketColor
+      ),
+      child: Center(
+        child: Text(
+          "Find tickets",
+          style: AppStyles.textStyle.copyWith(color: Colors.white, fontSize: 17),
+        ),
+      ),
+    );
+  }
+}
