9621653dbf9a4c74ad22dde07b619d21c5ba3449
Bohdan <protzzz.it@gmail.com>
2024-08-24 03:19:43 +0200

    Updated: search_screen

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/core/res/styles/app_styles.dart';
+import 'package:test_app/core/widgets/app_double_text.dart';
+import 'package:test_app/screens/search/widgets/app_text_icon.dart';
 import 'package:test_app/screens/search/widgets/app_ticket_tabs.dart';
+import 'package:test_app/screens/search/widgets/find_tickets.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -18,7 +23,47 @@ class SearchScreen extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           const AppTicketTabs(),
-
+          const SizedBox(height: 20),
+          const AppTextIcon(
+            icon: Icons.flight_takeoff_rounded,
+            text: "Departure",
+          ),
+          const SizedBox(height: 20),
+          const AppTextIcon(
+            icon: Icons.flight_land_rounded,
+            text: "Arrival",
+          ),
+          const SizedBox(height: 25),
+          const FindTickets(),
+          const SizedBox(height: 35),
+          AppDoubleText(
+            bigText: "Upcoming Flights",
+            smallText: "View All",
+            func: () => Navigator.pushNamed(context, "/all_tickets"),
+          ),
+          const SizedBox(height: 15),
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                width: size.width * .42,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 3,
+                        spreadRadius: 4
+                      )
+                    ]
+                ),
+                child: Column(
+                  children: [],
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
