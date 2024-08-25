2fc276096218c7997a4b082b69b12195fceae4b8
Bohdan <protzzz.it@gmail.com>
2024-08-25 22:01:35 +0200

    Added: new color settings

--- lib/core/widgets/text_style_third.dart
+++ lib/core/widgets/text_style_third.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:test_app/core/res/styles/app_styles.dart';
 
 class TextStyleThird extends StatelessWidget {
-  
   final String text;
-  const TextStyleThird({super.key, required this.text});
+  final bool? isColor;
+  const TextStyleThird({super.key, required this.text, this.isColor});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: AppStyles.headLineStyle2.copyWith(color: Colors.white),
+      style: isColor == null 
+        ? AppStyles.headLineStyle2.copyWith(color: Colors.white)
+        : AppStyles.headLineStyle2,
     );
   }
 }
