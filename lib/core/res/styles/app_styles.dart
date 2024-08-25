f68827cf78aca312f3dcecba65e3a2311a0751db
Bohdan <protzzz.it@gmail.com>
2024-08-25 22:01:22 +0200

    Added: new color

--- lib/core/res/styles/app_styles.dart
+++ lib/core/res/styles/app_styles.dart
@@ -12,6 +12,7 @@ class AppStyles {
   static Color planeColor = const Color(0xFFBFC2DF);
   static Color findTicketColor = const Color(0xd91130ce);
   static Color circleColor = const Color(0xFF189999);
+  static Color ticketColor = const Color(0xFFFFFFFF);
 
   static TextStyle textStyle =
       TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: textColor);
