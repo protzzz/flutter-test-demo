5cc8b9606775b15d6e215c4e44286c4aea161a29
Bohdan <protzzz.it@gmail.com>
2024-08-26 05:01:05 +0200

    Code fix

--- lib/core/widgets/app_routes.dart
+++ lib/core/widgets/app_routes.dart
@@ -1,5 +1,5 @@
 class AppRoutes {
   static const homePage = "/";
   static const allTickets = "/all_tickets";
-  static const ticketView = "/ticket_view";
+  static const ticketScreen = "/ticket_screen";
 }
