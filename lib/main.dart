218f69168d173c55d8b4dde8561611f378e6571a
Bohdan <protzzz.it@gmail.com>
2024-08-26 04:49:36 +0200

    Updated: new version of realization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/core/bottom_nav_bar.dart';
+import 'package:test_app/core/widgets/app_routes.dart';
 import 'package:test_app/screens/home/all_tickets.dart';
+import 'package:test_app/screens/ticket/ticket_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +17,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: const BottomNavBar(),
       routes: {
-        "/":(context) => const BottomNavBar(),
-        "/all_tickets":(context) => const AllTickets(),
+        // "/":(context) => const BottomNavBar(),
+        // "/all_tickets":(context) => const AllTickets(),
+        AppRoutes.homePage:(context) => const BottomNavBar(),
+        AppRoutes.allTickets:(context) => const AllTickets(),
+        AppRoutes.ticketView:(context) => const TicketScreen()
       },
     );
   }
