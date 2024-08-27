422506901a9605f3e9c14c2beff8d2c45432dcda
Bohdan <protzzz.it@gmail.com>
2024-08-27 17:28:19 +0200

    Updated: added new routes

--- lib/core/widgets/app_routes.dart
+++ lib/core/widgets/app_routes.dart
@@ -1,5 +1,7 @@
 class AppRoutes {
   static const homePage = "/";
   static const allTickets = "/all_tickets";
+  static const allHotels = "/all_hotels";
+  static const hotelDetail = "/hotel_detail";
   static const ticketScreen = "/ticket_screen";
 }
